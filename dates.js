d262e57b9c2a34390aec873d091a0c06112bdb9b
rushigittt <rushikeshmandavkar81@gmail.com>
2023-04-22 12:53:06 +0530

    date

---
+++ dates.js
@@ -0,0 +1,23 @@
+// const d1 = new Date();
+// console.log(`Date is: ${d1}`);
+// const d2 = new Date("October 13, 2014 11:13:00");
+// console.log(`Date is : ${d2}`);
+// const d3 = new Date(2018, 11, 24, 10, 33, 30, 0);
+// console.log(`Date is : ${d3}`);
+
+// const d = new Date(2015, 12);
+// console.log("Date is ", d);
+
+
+// const d = new Date(2015);
+// console.log("Date is ", d);
+
+// const d = new Date(99, 11, 24);
+// console.log("date is: ", d);
+
+// const d = new Date(1682143994533);
+// console.log("Date is :", d);
+
+const d = new Date("October 13, 2014 11:13:00");
+console.log(d.toUTCString());
+
